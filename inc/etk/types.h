17e9eb8c4fb6e033de95a4e68b79a947489b0b85
Samuel Cowen <samuel.cowen@camelsoftware.com>
2016-01-10 12:40:35 +1100

    Added types.h

---
+++ inc/etk/types.h
@@ -0,0 +1,45 @@
+#ifndef ETK_TYPES_H
+#define ETK_TYPES_H
+
+
+/*
+ *
+ *
+ */
+
+// 32bit PC
+typedef unsigned char byte ;
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int16;
+typedef unsigned short uint16;
+typedef int int32;
+typedef unsigned int uint32;
+typedef long int64;
+typedef unsigned long uint64;
+
+typedef double real_t;
+
+
+/*
+// 32bit ARM
+typedef unsigned char byte ;
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int16;
+typedef unsigned short uint16;
+typedef int int32;
+typedef unsigned int uint32;
+typedef long int64;
+typedef unsigned long uint64;
+
+typedef float real_t;
+*/
+
+// 8bit AVR
+
+
+
+#endif
+
+
