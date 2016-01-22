8c92044a95cf0507b04355af33bd4b5910e69b87
Samuel Cowen <samuel.cowen@camelsoftware.com>
2016-01-22 15:49:36 +1100

    Added stream.h

---
+++ inc/etk/stream.h
@@ -0,0 +1,74 @@
+/*
+    Embedded Tool Kit
+    Copyright (C) 2015 Samuel Cowen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef STREAM_H_INCLUDED
+#define STREAM_H_INCLUDED
+
+#include "rope.h"
+#include "string.h"
+#include "vector.h"
+#include "matrix.h"
+
+namespace etk
+{
+
+template <class derived> class Stream
+{
+public:
+    void print(const char* cstr)
+    {
+        while(*cstr != '\0')
+            static_cast<derived*>(this)->put(*cstr++);
+    }
+
+    template<typename T> void print(T v)
+    {
+        char buf[20];
+        etk::Rope rope(buf, 20);
+        rope << v;
+        for(uint32 i = 0; i < rope.length(); i++)
+            static_cast<derived*>(this)->put(buf[i]);
+    }
+
+    template<typename T, typename... Args> void print(T first, Args... args)
+    {
+        print(first);
+        print(args...);
+    }
+
+    Stream& operator << (auto& a)
+    {
+        print(a);
+        return *this;
+    }
+
+    Stream& operator << (const char* cstr)
+    {
+        print(cstr);
+        return *this;
+    }
+
+};
+
+
+}
+
+
+#endif
+
