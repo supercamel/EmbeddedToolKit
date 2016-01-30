36cbdf6e5a90c7b0f28e7514356ef4c80914800f
Samuel Cowen <samuel.cowen@camelsoftware.com>
2016-01-30 16:58:08 +1100

    Added version file

---
+++ inc/etk/version.h
@@ -0,0 +1,20 @@
+
+
+#ifndef ETK_AUTOVERSION_H_INCLUDED
+#define ETK_AUTOVERSION_H_INCLUDED
+
+namespace etk
+{
+
+namespace Version
+{
+	static const char* year = "16";
+	static const char* month = "1";
+	static const char* day = "30";
+	static const char* version = "160130";
+}
+
+}
+
+#endif
+
