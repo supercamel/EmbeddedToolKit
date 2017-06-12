823b78bbe092eda9920863b76cc3ce66cf8a1023
Samuel Cowen <samuel.cowen@camelsoftware.com>
2017-06-12 23:10:16 +1000

    updated comments

--- examples/statemachine/traffic.h
+++ examples/statemachine/traffic.h
@@ -25,9 +25,13 @@
  *
  * StateMachine uses a transition table to determine which events can change the
  * state and what the new state will be. 
- * { STATE::EW_YELLOW, STATE::DELAY, EVENT::DELAY_TIMEOUT, STATE::NS_GREEN },
+ * The format for each row is
+ * { last state, current state, event, next state }
+ *
+ * { STATE::EW_YELLOW, STATE::BOTH_RED, EVENT::BOTH_RED_TIMEOUT, STATE::NS_GREEN },
+ *
  * This line means if the last state was east/west light is yellow and the current 
- * state is delay, and there is a timeout event, then the new state must be 
+ * state is both red, and there is a timeout event, then the new state must be 
  * north/south light green. 
  * This example is quite trivial. Imagine if turning lanes, fault conditions and
  * pedestrian crossings needed to be implemented. 
@@ -44,7 +48,7 @@ enum class STATE
 {
 	EW_GREEN, //east/west light is green. north/south is red
 	EW_YELLOW, //east/west is yellow. ns is red.
-	DELAY, //both ways are red for a short moment
+	BOTH_RED, //both ways are red for a short moment
 	NS_GREEN, //ns is green, ew is red
 	NS_YELLOW, //ns is yellow, ew is red
 	END_STATE //END_STATE is mandatory. (boilerplate code)
@@ -55,7 +59,7 @@ enum class EVENT
 	EW_GREEN_TIMER, //this event occurs when the ew green light has been on for too long
 	EW_GREEN_TRAFFIC, //occurs when there is no traffic going through
 	EW_YELLOW_TIMER, //timer based event, so yellow light turns to red
-	DELAY_TIMEOUT, //delay timeout event, the delay is while both lights are red for a few seconds
+	BOTH_RED_TIMEOUT, //both red timeout event
 	NS_GREEN_TIMER, 
 	NS_GREEN_TRAFFIC,
 	NS_YELLOW_TIMER,
@@ -79,7 +83,7 @@ public:
 		//'external' events can be triggered by calling the 'submit_event()' function
 		add_event_check(EVENT::EW_GREEN_TIMER, &TrafficController::green_timer_check);
 		add_event_check(EVENT::EW_YELLOW_TIMER, &TrafficController::yellow_timer_check);
-		add_event_check(EVENT::DELAY_TIMEOUT, &TrafficController::delay_check);
+		add_event_check(EVENT::BOTH_RED_TIMEOUT, &TrafficController::both_red_check);
 		add_event_check(EVENT::NS_GREEN_TIMER, &TrafficController::green_timer_check);
 		add_event_check(EVENT::NS_YELLOW_TIMER, &TrafficController::yellow_timer_check);
 		
@@ -87,7 +91,7 @@ public:
 		//there are also exit callbacks for when a state is exited (use add_exit_callback() instead). 
 		add_entry_callback(STATE::EW_GREEN, &TrafficController::on_ew_green_entry);
 		add_entry_callback(STATE::EW_YELLOW, &TrafficController::on_ew_yellow_entry);
-		add_entry_callback(STATE::DELAY, &TrafficController::on_delay_entry);
+		add_entry_callback(STATE::BOTH_RED, &TrafficController::on_both_red_entry);
 		add_entry_callback(STATE::NS_GREEN, &TrafficController::on_ns_green_entry);
 		add_entry_callback(STATE::NS_YELLOW, &TrafficController::on_ns_yellow_entry);
 		
@@ -110,8 +114,8 @@ private:
 	void on_ew_yellow_entry()
 	{ std::cout << "Entered East/West yellow light state" << std::endl; }
 	
-	void on_delay_entry()
-	{ std::cout << "Entered delay state" << std::endl; }
+	void on_both_red_entry()
+	{ std::cout << "Entered both red state" << std::endl; }
 	
 	void on_ns_green_entry()
 	{ std::cout << "Entered North/South green state" << std::endl; }
@@ -129,7 +133,7 @@ private:
 		return(etk::now().diff_time(start) > 3.0);
 	}
 	
-	bool delay_check()
+	bool both_red_check()
 	{
 		return(etk::now().diff_time(start) > 2.0);
 	}
@@ -152,16 +156,16 @@ private:
 		{ STATE::END_STATE, STATE::EW_GREEN, EVENT::EW_GREEN_TIMER, STATE::EW_YELLOW },
 		//ignore last sate, if state is e/w green and there is no traffic, turn lights yellow
 		{ STATE::END_STATE, STATE::EW_GREEN, EVENT::EW_GREEN_TRAFFIC, STATE::EW_YELLOW },
-		//ignore last state, if east/west lights are yellow, and the timer goes off, turn all lights red (delay state)
-		{ STATE::END_STATE, STATE::EW_YELLOW, EVENT::EW_YELLOW_TIMER, STATE::DELAY },
-		//if last state was east/west yellow and state is delay and there is a timeout event, turn north south lights green
-		{ STATE::EW_YELLOW, STATE::DELAY, EVENT::DELAY_TIMEOUT, STATE::NS_GREEN },
-		//if last state was north/south yellow and state is delay and timeout occurs, turn east/west lights green
-		{ STATE::NS_YELLOW, STATE::DELAY, EVENT::DELAY_TIMEOUT, STATE::EW_GREEN },
+		//ignore last state, if east/west lights are yellow, and the timer goes off, turn all lights red
+		{ STATE::END_STATE, STATE::EW_YELLOW, EVENT::EW_YELLOW_TIMER, STATE::BOTH_RED },
+		//if last state was east/west yellow and state is both red and there is a timeout event, turn north south lights green
+		{ STATE::EW_YELLOW, STATE::BOTH_RED, EVENT::BOTH_RED_TIMEOUT, STATE::NS_GREEN },
+		//if last state was north/south yellow and state is both red and timeout occurs, turn east/west lights green
+		{ STATE::NS_YELLOW, STATE::BOTH_RED, EVENT::BOTH_RED_TIMEOUT, STATE::EW_GREEN },
 		// and so on . . .
 		{ STATE::END_STATE, STATE::NS_GREEN, EVENT::NS_GREEN_TIMER, STATE::NS_YELLOW },
 		{ STATE::END_STATE, STATE::NS_GREEN, EVENT::NS_GREEN_TRAFFIC, STATE::NS_YELLOW },
-		{ STATE::END_STATE, STATE::NS_YELLOW, EVENT::NS_YELLOW_TIMER, STATE::DELAY }
+		{ STATE::END_STATE, STATE::NS_YELLOW, EVENT::NS_YELLOW_TIMER, STATE::BOTH_RED }
 	};
 	
 	etk::Time start;
